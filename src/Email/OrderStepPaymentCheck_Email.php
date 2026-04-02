9163285be41be60d7cba81df17f33c6988014de7
SDGM <sion98madden@gmail.com>
2026-04-02 17:35:59 +1300

    FIX use namespace

--- src/Email/OrderStepPaymentCheck_Email.php
+++ src/Email/OrderStepPaymentCheck_Email.php
@@ -2,7 +2,7 @@
 
 namespace Sunnysideup\EcommercePaymentCheck\Email;
 
-use Order_Email;
+use Sunnysideup\Ecommerce\Email\Order_Email;
 use Sunnysideup\EcommercePaymentCheck\Email\OrderStepPaymentCheck_Email;
 
 
